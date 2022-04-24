52de9a092ee09d016301fa4fc7d2c0803b8451d0
EugeneYarkin <yarkin.eugene@gmail.com>
2022-04-24 11:39:06 +0500

    click on the button

--- app/src/main/java/com/eyarkin/loftmoneyjava2/AddItemActivity.java
+++ app/src/main/java/com/eyarkin/loftmoneyjava2/AddItemActivity.java
@@ -3,6 +3,7 @@ package com.eyarkin.loftmoneyjava2;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -11,14 +12,29 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddItemActivity extends AppCompatActivity {
-
+    static boolean flToast=true;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        Button button = findViewById(R.id.add_button);
+        Button button = (Button)findViewById(R.id.add_button);
         EditText textIncomes = findViewById(R.id.incomes_tv);
+        EditText textExpenses = findViewById(R.id.expenses_tv);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textIncomes.setText("");
+                textExpenses.setText("");
+                if (!flToast){
+                    Toast.makeText(getApplicationContext(),
+                            "Запрос отправлен!",
+                            Toast.LENGTH_SHORT).show();
+                    flToast=true;
+                }
+
+            }
+        });
         textIncomes.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -26,9 +42,14 @@ public class AddItemActivity extends AppCompatActivity {
             }
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                Toast.makeText(getApplicationContext(),
-                        "Пора покормить кота!",
-                        Toast.LENGTH_SHORT).show();  }
+                if (flToast){
+                    Toast.makeText(getApplicationContext(),
+                            "Данные введены!",
+                            Toast.LENGTH_SHORT).show();
+                    flToast=false;
+                }
+                }
+
 
             @Override
             public void afterTextChanged(Editable editable) {
--- app/src/main/java/com/eyarkin/loftmoneyjava2/MainActivity.java
+++ app/src/main/java/com/eyarkin/loftmoneyjava2/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         Intent activity = new Intent(this, AddItemActivity.class);
 
         startActivity(activity);
